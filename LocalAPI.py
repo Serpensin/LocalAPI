b280f26620cbff31d167eb7ccdd9793be84797fe
Serpensin <wissmann.matti@gmail.com>
2023-03-22 02:54:10 +0100

    Added html that lsits al json -files if opened in browser

--- LocalAPI.py
+++ LocalAPI.py
@@ -2,28 +2,37 @@ import json
 import tkinter as tk
 import os
 import sys
-from flask import Flask
+from flask import Flask, render_template_string
 from tkinter import filedialog, simpledialog
 
 
 app = Flask(__name__)
 
-@app.route('/<file>')
-def send_json(file):
-    filename = f'{file}.json'
-    try:
-        with open(os.path.join(path_to_json_files, filename)) as json_file:
-            data = json.load(json_file)
-    except FileNotFoundError:
-        return f"{filename} not found", 404
-    return data
+
+@app.route('/')
+def show_files():
+    path = app.config['PATH_TO_JSON_FILES']
+    files = [f for f in os.listdir(path) if f.endswith('.json')]
+    with open(os.path.join(app.static_folder, 'index.html')) as f:
+        html = f.read()
+    return render_template_string(html, files=files, current_path=path)
+
+@app.route('/<path:path>')
+def get_json_file(path):
+    path = os.path.join(app.config['PATH_TO_JSON_FILES'], f'{path}.json')
+    if os.path.isfile(path) and path.endswith('.json'):
+        with open(path) as f:
+            data = json.load(f)
+            return data
+    else:
+        return f"{path} not found or not a valid JSON file", 404
+
 
 if __name__ == '__main__':
     if len(sys.argv) < 2:
         root = tk.Tk()
         root.withdraw()
         path_to_json_files = filedialog.askdirectory(title="Select folder containing JSON files")
-        print(path_to_json_files)
         if not path_to_json_files:
             print("No folder selected. Exiting.")
             sys.exit(1)
--- build.bat
+++ build.bat
@@ -1,4 +1,4 @@
 @echo off
 set /p version="Enter Version: "
 
-./env/Scripts/nuitka.bat "LocalAPI.py" --plugin-enable=tk-inter --lto=yes --python-flag=no_site --plugin-enable=pylint-warnings --remove-output --standalone --onefile --company-name="Serpent Modding" --product-name="LocalAPI" --file-version=%version% --file-description="This serves locally saved json files." --windows-icon-from-ico="./icon.ico"
+./env/Scripts/nuitka.bat "LocalAPI.py" --plugin-enable=tk-inter --lto=yes --python-flag=no_site --plugin-enable=pylint-warnings --remove-output --standalone --onefile --company-name="Serpent Modding" --product-name="LocalAPI" --file-version=%version% --file-description="This serves locally saved json files." --windows-icon-from-ico="./icon.ico" --include-data-dir="./static/=static"
